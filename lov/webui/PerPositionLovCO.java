e02b79305e450c80a1e002fd910b195cb0d6e397
rei-gasis <41265292+rei-gasis@users.noreply.github.com>
2018-07-20 12:32:30 +0800

    Add files via upload

---
+++ lov/webui/PerPositionLovCO.java
@@ -0,0 +1,62 @@
+/*===========================================================================+
+ |   Copyright (c) 2001, 2005 Oracle Corporation, Redwood Shores, CA, USA    |
+ |                         All rights reserved.                              |
+ +===========================================================================+
+ |  HISTORY                                                                  |
+ +===========================================================================*/
+package xxup.oracle.apps.per.publicservice.lov.webui;
+
+import java.io.Serializable;
+
+import oracle.apps.fnd.common.VersionInfo;
+import oracle.apps.fnd.framework.OAApplicationModule;
+import oracle.apps.fnd.framework.webui.OAControllerImpl;
+import oracle.apps.fnd.framework.webui.OAPageContext;
+import oracle.apps.fnd.framework.webui.beans.OAWebBean;
+
+
+/**
+ * Controller for ...
+ */
+public class PerPositionLovCO extends OAControllerImpl
+{
+  public static final String RCS_ID="$Header$";
+  public static final boolean RCS_ID_RECORDED =
+        VersionInfo.recordClassVersion(RCS_ID, "%packagename%");
+
+  /**
+   * Layout and page setup logic for a region.
+   * @param pageContext the current OA page context
+   * @param webBean the web bean corresponding to the region
+   */
+  public void processRequest(OAPageContext pageContext, OAWebBean webBean)
+  {
+    super.processRequest(pageContext, webBean);
+    
+      OAApplicationModule am = pageContext.getApplicationModule(webBean);
+      
+      String employeeId = Integer.toString(pageContext.getEmployeeId());
+      
+      Serializable[] params = { employeeId };
+      
+      am.invokeMethod("initPosVO", params);
+      
+      //OAViewObject posVO = (OAViewObject)am.findViewObject("PerPositionsVO1");
+      
+      
+      
+      
+  }
+
+  /**
+   * Procedure to handle form submissions for form elements in
+   * a region.
+   * @param pageContext the current OA page context
+   * @param webBean the web bean corresponding to the region
+   */
+  public void processFormRequest(OAPageContext pageContext, OAWebBean webBean)
+  {
+    super.processFormRequest(pageContext, webBean);
+  }
+
+}
